f546df123495efc90f522463cffa9a98261cc00d
pawjer <pawel.jermalonek@polcode.net>
2021-06-18 13:27:01 +0200

    create service for family life simulation

--- app/services/family_life_simulation.rb
+++ app/services/family_life_simulation.rb
@@ -1,14 +1,26 @@
+
 class FamilyLifeSimulation
   include PixelMultiplicationLogic
 
   attr_reader :family, :timerange
 
-  def initialize(family, timerange)
+  def initialize(family, timerange:, randomize: true)
     @family = family
     @timerange = timerange
+    @randomize = randomize
+    @cycles = timerange / family.replication_time
   end
 
   def call
-    raise 'Not implemented yet'
+    puts "There will be #{@cycles} cycles"
+
+    @cycles.times do |i|
+      family.create_generation(randomize: @randomize)
+
+      printf("\rProgress: [%-100s]", '=' * (i / (@cycles / 100.0)))
+    end
+    printf("\rProgress: [%-100s]", '=' * 100)
+
+    family
   end
 end
